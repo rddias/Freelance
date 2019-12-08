ba2191d4e62e5926b847bf1331d077a28136456c
rdominguez <rdominguez@users.noreply.github.com>
2019-12-08 20:22:02 +0000

    small changes
    small changes

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 
     $("#header > .jumbotron").css({"height":remains});
 //    $("#about > .container-fluid").css({"height":remains});
-    $("#portifolio > .container-fluid").css({"height":remains});
+//    $("#portifolio > .container-fluid").css({"height":remains});
   }
 
   // On page load
@@ -23,52 +23,20 @@ $(document).ready(function () {
     adjustJumbotronHeight();
   });
   
-  
-  var i = 0;
-  var j = 0;
-//  var x = document.getElementsByClassName("test_class");
-//  $( "#test" ).hover(function() {
-//      i += 1;
-//      
-//     for (j in x) {
-//        $(x[j]).removeClass("");
-//        $(x[j]).addClass("col-12");        
-//      }
-//    
-//      $( this ).find( "span" ).text( "mouse over x " + i );
-//      $("#coluna").removeClass("col-4");
-//      $("#coluna").addClass("col-12");
-//    
-//    },
-//    function() {    
-//      for (j in x) {
-//        $(x[j]).removeClass("col-12");
-//        $(x[j]).addClass("col-4");        
-//      }
-//    
-//      $( this ).find( "span" ).text( "mouse out " );
-//      $("#coluna").removeClass("col-12");
-//      $("#coluna").addClass("col-4");
-//    });  
- 
   var x = document.getElementsByClassName("about_card");
-  $( "#test" ).hover(function() {
-      i += 1;
-      
-//     for (j in x) {
-//        $(x[j]).removeClass("flex-row");
-//        $(x[j]).addClass("flex-column");        
-//      }
-      $(x[j]).attr('style', 'width: 50px !important');
-//      $(x[j]).css({"padding-right": "110px"});
-    
+  $( "#responsive_card" ).hover(function() {
+      $(x[0]).attr('style', 'width: 50px !important');
     },
     function() {    
-//      for (j in x) {
-//        $(x[j]).removeClass("flex-column");
-//        $(x[j]).addClass("flex-row");        
-//      }
-      $(x[j]).attr('style', 'width: 100% !important');
+      $(x[0]).attr('style', 'width: 100% !important');
     });  
+  
+  var icon = document.getElementById("rocket_icon");
+  $( "#rocket" ).hover(function() {
+      $(icon).attr('style', 'height: 150px');
+    },
+    function() {    
+      $(icon).attr('style', 'height: 100px');
+    });
 });
 
